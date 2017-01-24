9011392602831c7c63860ca286f35d26024464ac
IonTuchila <tuchila.ion.1995@gmail.com>
2017-01-24 21:52:40 +0200

    redis exemple test

---
+++ src/main/java/md/utm/fi/runer/RedisStringJava.java
@@ -0,0 +1,18 @@
+package md.utm.fi.runer;
+
+/**
+ * Created by E7450 on 1/24/2017.
+ */
+import redis.clients.jedis.Jedis;
+public class RedisStringJava {
+    public static void main(String[] args) {
+        //Connecting to Redis server on localhost
+        Jedis jedis = new Jedis("localhost");
+        System.out.println("Connection to server sucessfully");
+        //set the data in redis string
+        jedis.set("tutorial-name", "Redis tutorial");
+        // Get the stored data and print it
+        System.out.println("Stored string in redis:: "+ jedis.get("tutorial-name"));
+
+    }
+}
